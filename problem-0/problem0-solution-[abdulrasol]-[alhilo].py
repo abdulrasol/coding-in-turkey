e28c580b65983576e6278241cf683372f12aa4e1
AbdulRasol S. Al-Hilo <abdulrsol97@gmail.com>
2021-10-27 13:14:33 +0300

    update solution 0 with comments

--- problem-0/problem0-solution-[abdulrasol]-[alhilo].py
+++ problem-0/problem0-solution-[abdulrasol]-[alhilo].py
@@ -1,5 +1,10 @@
+# get fride items from user and store it as list after removing unwanted chars
 fridge = input('input fridge item seperated by comma , as => banana, apple, milk: ').replace(' ','').split(',')
+
+# get elemet that uset want to find it and store it as string after removing unwanted chars
 elemnt = input('input element want to find it: ').lstrip().rstrip()
+
+# the function that responceble to find the element
 def whereIsMyFood(fridge, item):
     item_location = -1
     index = 0
